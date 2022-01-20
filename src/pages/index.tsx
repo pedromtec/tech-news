05739ba4c0a516e56dab0388932fa07edf9a61df
Pedro Soares <pedro.soares@pipefy.com>
2022-01-19 21:28:57 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tech news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+
+  )
+}
