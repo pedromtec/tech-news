5f6de9328cd45c37bd684e410ec5529b612f9a00
Pedro Soares <pedro.soares@pipefy.com>
2022-01-31 19:28:31 -0300

    Add stripe integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import { GetServerSideProps, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
+type HomeProps = {
+  product: {
+    priceId: string,
+    formattedAmount: string
+  }
+}
 
-export default function Home() {
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,10 +23,10 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.formattedAmount} month</span>
           </p>
-          <SubscribeButton />
-          
+          <SubscribeButton priceId={product.priceId} />
+
         </section>
 
         <img src="images/avatar.svg" alt="Girl coding" />
@@ -26,4 +34,27 @@ export default function Home() {
     </>
 
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const PRICE_ID = 'price_1KO7fJJIiuVyFNgm3VAKpUsg'
+
+  const price = await stripe.prices.retrieve(PRICE_ID)
+  const product = {
+    priceId: price.id,
+    formattedAmount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
+
 }
